031549db0c8ac26b86d5b29b2f61711de9947d30
Stephanie Sanchez <stephk006@gmail.com>
2022-06-08 14:30:31 -0400

    return a get request

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/todo-items", () => {});
+app.MapGet("/todo-items/{id}", (string id) => {
+    var todoItem = new {
+        Id = Guid.NewGuid().ToString(),
+        Title = "Complete Todo in C#",
+        IsCompleted = false
+    };
+    return Results.Ok(todoItem);
+});
+app.MapPost("/todo-items", () => {});
+app.MapPut("/todo-items/{id}", (string id) => {});
+app.MapDelete("/todo-items", (string id) => {});
 
 app.Run();
